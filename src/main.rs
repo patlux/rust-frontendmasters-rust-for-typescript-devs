7e5841c39e1c70eae85c3043aa38f08e506b62e0
Patrick Wozniak <email@patwoz.de>
2023-12-20 21:53:13 +0100

    Borrow Checker -> Stack vs Heap: Stack Overflow

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,11 @@
-fn practice(nums: Vec<usize>, index: usize) -> usize {
-    nums.get(index).unwrap_or(&index) * 5
-
-    // if let Some(nr) = nums.get(index) {
-    //     return nr * 5;
-    // }
-
-    // index * 5
+fn foo(nr: usize) {
+    println!("foo({})", nr);
+    foo(nr + 1)
 }
 
 fn main() {
-    println!("by value: {}.", practice(vec![1, 2, 3, 4], 2));
-    println!("by index: {}.", practice(vec![1, 2, 3, 4], 4));
+    foo(0);
 }
 
 #[cfg(test)]
-mod Tests {
-    use crate::practice;
-
-    #[test]
-    fn should_multiple_value() {
-        assert_eq!(practice(vec![1, 2, 3, 4], 2), 15)
-    }
-
-    #[test]
-    fn should_multiple_index() {
-        assert_eq!(practice(vec![1, 2, 3, 4], 4), 20)
-    }
-}
+mod Tests {}
