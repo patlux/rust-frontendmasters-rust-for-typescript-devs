4cd0f1d2400f6326cf035e79fda45f45938ab25c
Patrick Wozniak <email@patwoz.de>
2023-12-20 20:48:41 +0100

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+struct Custom {
+    age: usize,
+    name: String,
+}
+
+enum Item {
+    Number(usize),
+    String(String),
+    MyCustom(Custom),
+}
+
+fn append(items: &mut Vec<Item>) {
+    items.push(Item::Number(2))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let foo = Item::MyCustom(Custom {
+        age: 30,
+        name: "Patrick".into(),
+    });
+
+    let mut items: Vec<_> = vec![
+        Item::Number(2),
+        Item::String("Patrick".into()),
+        Item::MyCustom(Custom {
+            age: 30,
+            name: "Patrick Wozniak".into(),
+        }),
+    ];
+
+    append(&mut items);
 }
